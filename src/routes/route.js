8ed3e8f193eeb1dd18bb0f33f9a14e0e502aee10
Mayank Mohan <swaggerb666@gmail.com>
2022-06-03 01:19:01 +0530

    Updated Assignment given on 01 June

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require('lodash')
 const externalModule = require('../logger/logger')
 const externalModuleTwo = require('../util/helper')
 const externalModuleThree = require('../validatter/formatter')
@@ -16,21 +17,17 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.get('/test-me1', function (req, res) {
+router.get('/hello', function (req, res) {
+    let Months = lodash.chunk(['January','February','March','April','May','June','July','August','September','October','November','December'],3)
+    console.log(Months)
+   let OddNumbers = lodash.tail([1,3,5,7,9,11,13,15,17,19])
+   console.log(OddNumbers)
+   let arr = lodash.union([1,2,3], [4,2,34], [7,6,33], [7,34,10])
+   console.log(arr)
+   let cricketers = [['Batsman','Virat Kohle'],['Bowler','Jasprit Bumrah'],['All Rounder','Hardik Pandya'],['Captain','Rohit Sharma']]
+   let obj = lodash.fromPairs(cricketers)
+   console.log(obj)
     res.send('My second ever api!')
 });
-
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
-
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
-
 module.exports = router;
 // adding this comment for no reason
