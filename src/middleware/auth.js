e16b96a1fcfa6e3ab011439864f66c1112f4c2c9
Mayank Mohan <swaggerb666@gmail.com>
2022-06-16 07:16:40 +0530

    auth-2 assignment

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,12 @@
 const authenticate = function(req, req, next) {
     //check the token in request header
+    let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+  if (!token) return res.send({ status: false, msg: "token must be present" })
     //validate this token
+    let decodedToken = jwt.verify(token, "functionup-thorium");
+  if (!decodedToken)
+    return res.send({ status: false, msg: "token is invalid" })
 
     next()
 }
@@ -8,5 +14,14 @@ const authenticate = function(req, req, next) {
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+    let userToBeModified = req.params.userId
+    let userLoggedIn = decodedToken.userId
+    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+
+    let user = await userModel.findById(req.params.userId)
+    if(!user) return res.send({status: false, msg: 'No such user exists'})
     next()
 }
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
